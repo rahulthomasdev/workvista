ed8e983c040dbb3b4f51161c569bd256781c0b76
Rahul <rahult808680@gmail.com>
2023-11-25 22:45:25 +0530

    initial commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="main-page">
+        <nav id="navbar">
+          <h1 className="logo">
+            Work<span>Vista</span>
+          </h1>
+
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/jobs">Jobs</Link>
+            </li>
+            <li>
+              <Link to="/saved-job">Wishlist</Link>
+            </li>
+            <li>
+              <Link to="/post-job">Advertise</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
